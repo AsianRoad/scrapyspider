2354cd13a99c7468eb58f5b0942c0ac6b72223b3
AsianRoad <wzc1416353344@outlook.com>
2016-10-10 19:37:22 +0800

    add annotation

--- scrapyimage/tutorial/tutorial/spiders/imagespider.py
+++ scrapyimage/tutorial/tutorial/spiders/imagespider.py
@@ -9,14 +9,18 @@ class imagespider(scrapy.Spider):
     name = 'lofter'
     allowed_domains = []
     start_urls = ["http://www.duitang.com/blog/?id=645169956"]
+    #请求页面
 
     def parse(self,response):
         item = MyItem()
         item['image_urls'] = response.xpath('//img[@id="mbpho-img"]//@src').extract()
+        #爬取页面中图片，在mbpho-img中
 
         yield item
 
         new_url = "http://www.duitang.com"+response.xpath('//a[@class="shownext"]//@href').extract_first()
+        #获取下一页的url
 
         if new_url:
             yield  scrapy.Request(new_url,callback=self.parse)
+            #请求要爬取的下一个页面
--- scrapyproject/tutorial/tutorial/spiders/dmoz_spider.py
+++ scrapyproject/tutorial/tutorial/spiders/dmoz_spider.py
@@ -11,6 +11,7 @@ class DmozSpider(scrapy.Spider):
     name = "dmoz"
     allowed_domains = []
     start_urls = ["https://vpn2.seu.edu.cn/dana-na/auth/url_default/welcome.cgi"]
+    #请求登陆页面
 
 
     def parse(self,response):
@@ -19,12 +20,14 @@ class DmozSpider(scrapy.Spider):
             formdata= {'username':'220163689','password':'519931116477x2'},
             callback=self.after_login
         )
+    #提交登陆表单
 
     def after_login(self,response):
         return  Request(
             "https://vpn2.seu.edu.cn/,DanaInfo=s.g.wanfangdata.com.cn+Paper.aspx?q=%E6%95%B0%E6%8D%AE%E6%8C%96%E6%8E%98",
             callback= self.parse_page
         )
+    #跳转到要爬取的页面
 
     def parse_page(self,response):
 
@@ -32,6 +35,7 @@ class DmozSpider(scrapy.Spider):
             item = DmozItem()
             item['title'] = site.xpath('li[@class="title_li"]/text()').extract()
             item['link'] = site.xpath('li[@class="title_li"]/a[2]/@href').extract()
+            #爬去论文标题链接摘要等
 
 
             yield  item
